0e4103af13f5fea49062484006ce8b42fa02a1a4
christinawkhau <christinawkhau2015@gmail.com>
2025-10-13 17:54:14 +0800

    Step 17 : register/logout

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,7 @@ load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = os.getenv('SITE_SECRET_KEY')
+SECRET_KEY = os.getenv('SITE_SECRET_KEY')  #server signature where we hide, SITE KEY+USERNAME+PASSWORD ==> KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
